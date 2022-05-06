22705b95584ecf2d5a120f266e9480c9055f43d7
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2022-05-05 22:17:57 -0700

    homework done

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,9 +1,14 @@
-import MeetupItem from './MeetupItem';
-import classes from './MeetupList.module.css';
+import Grid from "@mui/material/Grid";
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
 
 function MeetupList(props) {
   return (
-    <ul className={classes.list}>
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 1, sm: 8, md: 12 }}
+    >
       {props.meetups.map((meetup) => (
         <MeetupItem
           key={meetup.id}
@@ -13,7 +18,18 @@ function MeetupList(props) {
           address={meetup.address}
         />
       ))}
-    </ul>
+    </Grid>
+    // <ul className={classes.list}>
+    //   {props.meetups.map((meetup) => (
+    //     <MeetupItem
+    //       key={meetup.id}
+    //       id={meetup.id}
+    //       image={meetup.image}
+    //       title={meetup.title}
+    //       address={meetup.address}
+    //     />
+    //   ))}
+    // </ul>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
 // this is like root page in react
-
+import React from "react";
+import { wrapper, store } from "../redux/store";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
+import { Provider } from "react-redux";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
